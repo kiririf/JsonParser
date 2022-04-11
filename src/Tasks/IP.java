1ffb73623ef09564a4dfab13e22536efa7261079
Егор <lynsjef@gmail.com>
2022-04-11 16:46:52 +0300

    init

---
+++ src/Tasks/IP.java
@@ -0,0 +1,21 @@
+package Tasks;
+
+public class IP {
+
+    private String ip;
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    @Override
+    public String toString() {
+        return "IP{" +
+                "ip='" + ip + '\'' +
+                '}';
+    }
+}
---
+++ src/Tasks/Task2.java
@@ -0,0 +1,30 @@
+package Tasks;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.jsoup.Jsoup;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+
+public class Task2 {
+    public static void main(String[] args) throws IOException {
+        String ipReader = Jsoup.connect("https://api.ipify.org/?format=json") //подключаемся по ссылке и получаем данные
+                .ignoreContentType(true).execute().body();
+
+        GsonBuilder builder = new GsonBuilder();
+        Gson parser = builder.create();//создаём парсер, который преобразует Json в
+
+        IP catcher = parser.fromJson(ipReader, IP.class);//переводим Json в объект
+        ipInFile(catcher);//функция записи в файл
+    }
+
+    private static void ipInFile(IP catcher){
+        try (FileWriter writeIp = new FileWriter("ipBarn.txt")){//отлавливаем исключения
+            writeIp.write("ip ползователя:" + catcher.getIp());
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
