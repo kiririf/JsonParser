07fb3ab0e0a8c02d2ccda5bd243876aef015c565
Егор <lynsjef@gmail.com>
2022-04-19 15:05:51 +0300

    init

--- src/com/tn/educationtask2/Task2.java
+++ src/com/tn/educationtask2/Task2.java
@@ -17,7 +17,7 @@ public class Task2 {
             try {
                 strategyType = StrategyTypeEnum.valueOf(inputType.toUpperCase()).chosenPath();
             } catch (RuntimeException e) {
-                System.out.println("Enter the correct input type");
+                throw new RuntimeException("Enter the correct input type");
             }
             JsonToIPParser jsonToIp = new JsonToIPParser();
             jsonToIp.setGetIpStrategy(strategyType);
